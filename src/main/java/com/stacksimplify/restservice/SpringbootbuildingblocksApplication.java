d5d5c05e3e6ad5a4963e9eaa1340a3ca4e3f5411
Mani Manavalan <Mani.Manavalan@non.se.com>
2020-08-16 00:53:41 -0400

    First Commit

---
+++ src/main/java/com/stacksimplify/restservice/SpringbootbuildingblocksApplication.java
@@ -0,0 +1,14 @@
+package com.stacksimplify.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbuildingblocksApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringbootbuildingblocksApplication.class, args);
+	}
+
+}
